ae1d137f6c2721e9727624ba6bb1336c8ffbec7e
Profpatsch <mail@profpatsch.de>
2014-07-21 03:44:45 +0200

    Basic grid template working.

--- beekeeper/Foundation.hs
+++ beekeeper/Foundation.hs
@@ -75,7 +75,8 @@ instance Yesod App where
         pc <- widgetToPageContent $ do
             $(combineStylesheets 'StaticR
                 [ css_normalize_css
-                , css_bootstrap_css
+                , css_bootstrap_min_css
+                , css_bootstrap_theme_min_css
                 ])
             $(widgetFile "default-layout")
         giveUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
--- beekeeper/Handler/BeeList.hs
+++ beekeeper/Handler/BeeList.hs
@@ -1,6 +1,16 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Handler.BeeList where
 
 import Import
+import Beehive
 
 getBeeListR :: Handler Html
-getBeeListR = error "Not yet implemented: getBeeListR"
+getBeeListR = defaultLayout $ do
+  setTitle "Lemonade"
+  $(widgetFile "bees") 
+    where bees = [bee1, bee2, bee3]
+          bee1 = Bee "Default" "DefaultClass" [] "Description"
+          bee2 = defaultBee
+          bee3 = bee1
